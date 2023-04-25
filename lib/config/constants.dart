97307115d8cc8e383b85a691c5bd9ec8e9fc4c7c
Zmicier Szkiel <zmicierszkiel@gmail.com>
2023-04-25 08:47:13 +0300

    config added

---
+++ lib/config/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const String AppName = 'PokeApp';
+const double MaxGridExtent = 200;
+
+class AppColours {
+  static const primaryColour = Color.fromARGB(0, 53, 88, 205);
+  static const scaffoldColour = Color.fromARGB(0, 232, 232, 232);
+  static const textBodyColour = Color.fromARGB(0, 107, 107, 107);
+  static const textPrimaryColour = Color.fromARGB(0, 22, 26, 51);
+}
+
+class LocalDbKeys {
+  static const favouriteDb = 'favouriteDb';
+}
---
+++ lib/config/theme.dart
@@ -0,0 +1,13 @@
+import 'package:new_pokemon_app/config/constants.dart';
+import 'package:flutter/material.dart';
+
+final lightThemeData = ThemeData(
+  primaryColor: AppColours.primaryColour,
+  scaffoldBackgroundColor: AppColours.scaffoldColour,
+);
+
+class Insets {
+  static const double xs = 4;
+  static const double sm = 8;
+  static const double md = 16;
+}
